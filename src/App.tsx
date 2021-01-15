e32a9b6add69edbcbe85b293b7d93ad4258ea795
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2021-01-15 07:59:38 -0300

    developing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import img from './assets/img.jpg';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  console.log(isLoading);
   return (
-    <div>
-      <img src={img} className='app__img' />
+    <div className='app__container'>
+      {isLoading && <div>Loading</div>}
+      <img 
+        style={isLoading ? {display: 'none'} : {}}
+        className='app__img'
+        src='https://source.unsplash.com/collection/1/1600x900'
+        // src={img}
+        alt='company'
+        onLoad={() => setIsLoading(false)}
+      />
     </div>
   );
 };
